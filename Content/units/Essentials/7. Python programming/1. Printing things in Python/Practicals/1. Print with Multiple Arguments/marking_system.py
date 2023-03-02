f633cd65aa7eb0eb4f721f207751394ddc8c20cf
IvanYingX <ying.yingxuan@hotmail.com>
2023-03-02 10:42:15 +0000

    Update public copy

--- Content/units/Essentials/7. Python programming/1. Printing things in Python/Practicals/1. Print with Multiple Arguments/marking_system.py
+++ Content/units/Essentials/7. Python programming/1. Printing things in Python/Practicals/1. Print with Multiple Arguments/marking_system.py
@@ -2,7 +2,7 @@ def check_step_1(
     age: int,
 ) -> None:
     assert isinstance(age, int), "The variable 'age' should be an integer."
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_2(
     your_code: str,
@@ -10,13 +10,13 @@ def check_step_2(
     assert "Age" in your_code, "Your print statement should contain the word 'Age'"
     assert "Age:" in your_code, "Your print statement should contain the word 'Age' followed by a colon"
     assert "age" in your_code, "Your print statement should contain the variable 'age'"
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_3(
     name: str,
 ) -> None:
     assert isinstance(name, str), "The variable 'name' should be a string."
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_4(
     your_code: str,
@@ -25,4 +25,4 @@ def check_step_4(
     assert "name" in your_code, "Your print statement should contain the variable 'name'"
     assert "and I am" in your_code, "Your print statement should contain the phrase 'and I am'"
     assert "age" in your_code, "Your print statement should contain the variable 'age'"
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
--- Content/units/Essentials/7. Python programming/2. Variables/Practicals/0. Variable Naming/marking_system.py
+++ Content/units/Essentials/7. Python programming/2. Variables/Practicals/0. Variable Naming/marking_system.py
@@ -3,25 +3,25 @@ def check_step_1(
 ) -> None:
     assert isinstance(my_variable, int), "The variable 'age' should be an integer."
     assert my_variable == 23, "The variable 'age' should be equal to 23."
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_2(
     your_code: str,
 ) -> None:
     assert "print" in your_code, "Your code should contain a print statement."
     assert "my_variable" in your_code, "Your print statement should contain the variable 'my_variable'"
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_3(
     my_variable: int,
 ) -> None:
     assert isinstance(my_variable, int), "The variable 'age' should be an integer."
     assert my_variable == 98, "The variable 'age' should be equal to 98."
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_4(
     your_code: str,
 ) -> None:
     assert "print" in your_code, "Your code should contain a print statement."
     assert "my_variable" in your_code, "Your print statement should contain the variable 'my_variable'"
-    print("Well done!")
+    print("\033[92m\N{heavy check mark} Well done!")
