b89312b0247ae2ce55b1940e80f299d434704621
IvanYingX <ying.yingxuan@hotmail.com>
2023-03-20 16:01:29 +0000

    Update public copy

--- Content/units/Data-Handling/3. SQL/4. SQL best practices/Imports/video_player.py
+++ Content/units/Data-Handling/3. SQL/4. SQL best practices/Imports/video_player.py
@@ -4,6 +4,6 @@ from base64 import b64encode
 def play_video(video_name):
     mp4 = open(f"/content/{video_name}",'rb').read()
     mp4 = f"data:video/mp4;base64,{b64encode(mp4).decode()}"
-    display.HTML(f"""<video width="60%" height="60%" controls
+    return display.HTML(f"""<video width="60%" height="60%" controls
         src={mp4} >
     </video>""")
