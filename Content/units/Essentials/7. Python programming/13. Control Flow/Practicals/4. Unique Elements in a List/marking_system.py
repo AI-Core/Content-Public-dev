73d086c4eda95d67caf15e939b0195e63633f352
IvanYingX <ying.yingxuan@hotmail.com>
2023-03-20 16:55:59 +0000

    Update public copy

--- Content/units/Essentials/7. Python programming/13. Control Flow/Practicals/4. Unique Elements in a List/marking_system.py
+++ Content/units/Essentials/7. Python programming/13. Control Flow/Practicals/4. Unique Elements in a List/marking_system.py
@@ -29,9 +29,9 @@ def check_step_1(
         ("Your code does not print the case when all elements are unique. "
          "If it does, make sure you are printing 'All elements are unique'. "
          "Please, try again.")
-    assert "There are duplicate elements in the list" in users_code, \
-        ("Your code does not print the case when there are duplicate elements in the list. "
-         "If it does, make sure you are printing 'There are duplicate elements in the list'. "
+    assert "There are duplicate elements" in users_code, \
+        ("Your code does not print the case when there are duplicate elements. "
+         "If it does, make sure you are printing 'There are duplicate elements'. "
          "Please, try again.")
     print(
         "\033[92m\N{heavy check mark} Well done! "
