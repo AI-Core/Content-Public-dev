e78de7be4c321a3db50c8777175316e816d48337
IvanYingX <ying.yingxuan@hotmail.com>
2023-03-23 20:10:47 +0000

    Update public copy

--- Content/units/Data-Handling/3. SQL/2. SQL Tools Setup/Imports/video_player.py
+++ Content/units/Data-Handling/3. SQL/2. SQL Tools Setup/Imports/video_player.py
@@ -4,6 +4,6 @@ from base64 import b64encode
 def play_video(video_name):
     mp4 = open(f"/content/{video_name}",'rb').read()
     mp4 = f"data:video/mp4;base64,{b64encode(mp4).decode()}"
-    return display.HTML(f"""<video controls autoplay
+    return display.HTML(f"""<video height=50% width=50% controls autoplay
         src={mp4} >
     </video>""")
