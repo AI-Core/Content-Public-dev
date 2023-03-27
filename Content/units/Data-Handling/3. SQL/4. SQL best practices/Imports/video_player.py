8116eab07beed58672c59c0860735db443bc5f92
IvanYingX <ying.yingxuan@hotmail.com>
2023-03-27 15:01:53 +0000

    Update public copy

--- Content/units/Data-Handling/3. SQL/4. SQL best practices/Imports/video_player.py
+++
@@ -1,9 +0,0 @@
-from IPython import display
-from base64 import b64encode
-
-def play_video(video_name):
-    mp4 = open(f"/content/{video_name}",'rb').read()
-    mp4 = f"data:video/mp4;base64,{b64encode(mp4).decode()}"
-    return display.HTML(f"""<video controls autoplay
-        src={mp4} >
-    </video>""")
