c6bdbc1effd302b5aae7bf6a9bf5c65f1b048c54
IvanYingX <ying.yingxuan@hotmail.com>
2023-08-03 10:50:30 +0000

    Update public copy

--- Content/units/Essentials/7. Python programming/22. Lambda Functions/Practicals/1. Sorting with Lambda Functions/marking_system.py
+++ Content/units/Essentials/7. Python programming/22. Lambda Functions/Practicals/1. Sorting with Lambda Functions/marking_system.py
@@ -5,42 +5,41 @@ def check_step_1(
 ) -> None:
     assert all(type(i)==tuple for i in data), ("The list should only contain tuples")
     assert all(len(i)==2 for i in data), ("The tuples should all contain 2 values")
+    assert all(type(i[0])==str for i in data), ("The first element of each tuple should be a string")
+    assert all(type(i[1])==int for i in data), ("The second element of each tuple should be an integer")
 
     print(
         "\033[92m\N{heavy check mark} Well done! "
         "You initialised the data correctly.")
     
 def check_step_2(
-    data: list, 
-    sort_num: Callable[[tuple], str]
+    data: list
 ) -> None:
-    assert isinstance(sort_num, type(lambda x:x)), "The function should utilise a lambda function"
-    assert data==sorted(data, key=sort_num), ("The list should be sorted using the sort_num lambda as a key")
+    sort_num = lambda x : x[1]
+    assert data==sorted(data, key=sort_num), ("The data isn't sorted correctly")
 
     print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_3(
-    data: list, 
-    sort_name: Callable[[tuple], str]
+    data: list
 ) -> None:
-    assert isinstance(sort_name, type(lambda x:x)), "The function should utilise a lambda function"
-    assert data==sorted(data, key=sort_name), ("The list should be sorted using the sort_name lambda as a key")
+    sort_name = lambda x : x[0]
+    assert data==sorted(data, key=sort_name), ("The data isn't sorted correctly")
 
     print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_4(
-    data: list, 
-    sort_name_len: Callable[[tuple], int]
+    data: list
 ) -> None:
-    assert isinstance(sort_name_len, type(lambda x:x)), "The function should utilise a lambda function"
-    assert data==sorted(data, key=sort_name_len), ("The list should be sorted using the sort_name_len lambda as a key")
+    sort_name_len = lambda x : len(x[0])
+    assert data==sorted(data, key=sort_name_len), ("The data isn't sorted correctly")
 
     print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_5(
-    data: list, 
-    sort_name_len: Callable[[tuple], int]
+    data: list
 ) -> None:
-    assert data==sorted(data, key=sort_name_len)[::-1], ("The list should be sorted backwards using the sort_name_len lambda as a key")
+    sort_name_len = lambda x : len(x[0])
+    assert data==sorted(data, key=sort_name_len)[::-1], ("The data isn't sorted correctly")
 
     print("\033[92m\N{heavy check mark} Well done!")
--- Content/units/Essentials/7. Python programming/22. Lambda Functions/Practicals/3. Lambda Functions with Filter/marking_system.py
+++ Content/units/Essentials/7. Python programming/22. Lambda Functions/Practicals/3. Lambda Functions with Filter/marking_system.py
@@ -10,27 +10,38 @@ def check_step_1(
         "You initialised the data correctly.")
 
 def check_step_2(
-    length_check: Callable[[str], bool],
-    data: list,
-    data_old: list
+    length_check: Callable[[int], int],
+    data_old: list,
+    data_new: list
 ) -> None:
     
-    assert isinstance(length_check, type(lambda x:x)), ("The function should utilise a lambda function")
-    assert length_check('hello') == False, ("Lambda should evaluate for longer than 5 characters, not longer than or equal to 5 characters")
-    assert data == list(filter(length_check, data_old)), ("You need to use the filter function on the data")
+    length_check_test = lambda x : len(x) > 5
+    data_test = list(filter(length_check_test, data_old))
+    
+    assert isinstance(length_check, type(lambda x:x)), "The function 'length_check' should be a lambda function"
+    assert length_check('apples')==True, "The function 'length_check' should return True for strings longer than 5 characters"
+    assert length_check('eggs')==False, "The function 'length_check' should return False for strings that aren't longer than 5 characters"
+    assert length_check('fruit')==False, "The function 'length_check' should return False for strings that are shorter than or equal to 5 characters"
+    assert type(data_new)!=filter, "You need to cast the filter object to a list"
+    assert data_new == data_test, "The function 'length_check' needs to filter out the elements in the list"
 
     print("\033[92m\N{heavy check mark} Well done!")
 
 def check_step_3(
-    length_vowel_check: Callable[[str], bool],
-    data: list,
-    data_old: list
+    length_vowel_check: Callable[[int], int],
+    data_old: list,
+    data_new: list
 ) -> None:
     
-    assert isinstance(length_vowel_check, type(lambda x:x)), ("The function should utilise a lambda function")
-    assert length_vowel_check('hello') == False, ("Lambda should evaluate 'True' for strings longer than 5 characters, not longer than or equal to 5 characters")
-    assert length_vowel_check('bananas') == False, ("Lambda should evaluate 'True' for strings longer than 5 characters that begin with a vowel")
-    assert length_vowel_check('apple') == False, ("Lambda should evaluate 'False' for strings less than 5 characters that begin with a vowel")
-    assert data == list(filter(length_vowel_check, data_old)), ("You need to use the filter function on the data")
+    length_vowel_check_test = lambda x : (len(x) > 5) and (x[0].lower() in 'aeiou')
+    data_test = list(filter(length_vowel_check_test, data_old))
+    
+    assert isinstance(length_vowel_check, type(lambda x:x)), "The function 'length_check' should be a lambda function"
+    assert length_vowel_check('apples')==True, "The function 'length_vowel_check' should return True for strings longer than 5 characters that begin with a vowel"
+    assert length_vowel_check('apple')==False, "The function 'length_vowel_check' should return False for strings shorter than 5 characters that begin with a vowel"
+    assert length_vowel_check('bananas')==False, "The function 'length_vowel_check' should return False for strings longer than 5 characters that don't begin with a vowel"
+    assert length_vowel_check('bob')==False, "The function 'length_vowel_check' should return False for strings shorter than 5 characters that don't begin with a vowel"
+    assert type(data_new)!=filter, "You need to cast the filter object to a list"
+    assert data_new == data_test, "The function 'length_vowel_check' needs to filter out the elements in the list"
 
     print("\033[92m\N{heavy check mark} Well done!")
